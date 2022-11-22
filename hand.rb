271b4a297f7f2274b5542dbc6c7b138d0fa0d65f
Ephfullstack <Ephron.Agyemann@gmail.com>
2022-11-22 12:07:13 +0000

    created methods which can be used to order cards into winning hands

--- hand.rb
+++ hand.rb
@@ -8,9 +8,35 @@ class Hand
     @cards = hand_array.map { |card| Card.new(card[:value], card[:suit]) }
   end
 
-  # simple method which retruns hand as an array 
+  # simple method which returns hand as an array 
   def to_a
     hand_array
   end 
+
+  #method retuns suit of all cards in hand 
+  def suits
+    card.map(&:Suit)
+  end 
+
+  #this method returns values of all cards in an hand 
+  def rank_values
+    cards.map(&:rank_value).sort
+  end
+
+  # this method return card value with no of occurances as an key value hash.
+  def rank_count_hash
+    rank_values.each_with_object(Hash.new(0)) { |value, count| count[value] += 1 }
+  end
+
+   # method return no of occurrences of a value of a card as an array.
+
+   def rank_count_totals
+    rank_count_hash.values
+   end
+
+  
+
+
+
 
 end 
