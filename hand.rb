ae1f930b18e4ed13a0348d8923f562b222a8d6b7
Ephfullstack <Ephron.Agyemann@gmail.com>
2022-11-22 12:17:51 +0000

    created method scoring hands as well as 10 methods which return the rank of a hand based on poker rules as an integer value

--- hand.rb
+++ hand.rb
@@ -34,6 +34,44 @@ class Hand
     rank_count_hash.values
    end
 
+   # this method return hands rank methods and each method checks rank of a hand bases on poker rules.
+
+  def scoring_hands
+    [high_card?, one_pair?, two_pair?, three_of_a_kind?, five_high_straight?,
+     straight?, flush?, full_house?, four_of_a_kind?, straight_flush?]
+  end
+
+  # below 10 methods return rank of a hand based on pokker rules as an integer value.
+  def high_card?
+    rank_count_totals.max == 1
+  end
+  def one_pair?
+    rank_count_totals.max == 2
+  end
+  def two_pair?
+    rank_count_totals.sort == [1, 2, 2]
+  end
+  def three_of_a_kind?
+    rank_count_totals.max == 3
+  end
+  def five_high_straight?
+    rank_values.sort == [2, 3, 4, 5, 14]
+  end
+  def straight?
+    rank_values.each_cons(2).all? { |a, b| a + 1 == b }
+  end
+  def flush?
+    suits.uniq.count == 1
+  end
+  def full_house?
+    rank_count_totals.sort == [2, 3]
+  end
+  def four_of_a_kind?
+    rank_count_totals.max == 4
+  end
+  def straight_flush?
+    straight? && flush?
+  end
   
 
 
