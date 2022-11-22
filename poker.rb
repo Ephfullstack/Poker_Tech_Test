92c117b2fca2a728eaae99248b459797085fc0ce
Ephfullstack <Ephron.Agyemann@gmail.com>
2022-11-22 13:06:33 +0000

    craeated rank_hands method whichranks hand based on score

--- poker.rb
+++ poker.rb
@@ -6,4 +6,14 @@ class Poker
       # Initializing Hand class instances from the hands json data.
       @hands = hand_arrays.map { |hand_array| Hand.new(hand_array) }
     end
+
+    # rank hands method ranks hands basesd on their score.
+
+  def rank_hands
+    hands.sort_by{ |hand| hand.score }.reverse.map do |h|
+      puts "Rank: #{h.hand_score}, Card:#{h.cards.inspect}"
+    end
+  end
+
+
 end
